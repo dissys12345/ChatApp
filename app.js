a8c0dc7cee987f53d1a8aa1f2ae4617d76943dee
pimta <pimtapim@hotmail.com>
2018-05-01 19:05:13 +0700

    2

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res) {
 
 users = [];
 var userdict = {};
+var messages  = [];
+var storeMessage = function(name, data){
+  messages.push({name: name, data: data});
+};
 io.on('connection', function(socket) {
    console.log('A user connected');
    socket.on('setUsername', function(data) {
@@ -17,8 +21,8 @@ io.on('connection', function(socket) {
          socket.emit('userExists', data + ' username is taken! Try some other username.');
       } else {
          users.push(data);
-         socket.emit('userSet', {username: data});
-         userdict[data] = socket.id;
+         socket.emit('userSet', {user: data});
+         userdict[socket.id] = data.user;
          console.log("socket id : " + socket.id + " is mapped to username : " + data);
       }
    });
@@ -27,16 +31,29 @@ io.on('connection', function(socket) {
       console.log("event groupid");
       console.log("group = " + data.group);
       socket.join(data.group);
-      console.log(data.username +" joins group "  +data.group);
-      io.sockets.in(data.group).emit('newmsg',{message:data.username +" joins group "  +data.group, user:""});
+      console.log(data.user +" joins group "  +data.group);
+      io.sockets.in(data.group).emit('newmsg',{message:data.user +" has joined group "  +data.group, user:""});
       socket.emit('connectToRoom', "joined group " + data.group)
+   });
+   socket.on('leaveGroup', function(data){
+     
+      console.log("try to leave group : " + data.group);
+      socket.leave(data.group);
+      console.log(data.user +" left group "  +data.group);
+      io.sockets.in(data.group).emit('newmsg',{message:data.user +" left group "  + data.group, user:""});
+      
+      socket.emit('userSet', userdict[socket.id] + data.group);
    });
 
    socket.on('msg', function(data) {
       //Send message to everyone
       io.sockets.in(data.group).emit('newmsg', data);
-
+       storeMessage(data.user, data.message);
+       console.log(data.user + ': ' + data.message);
    });
+
 });
 
-http.listen(process.env.PORT || 3000);
+http.listen(3000, function() {
+   console.log('listening on localhost:3000');
+});
